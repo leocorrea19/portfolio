60d050ba7e47b3151bc81121dd60cb2ae354b550
Leonel Javier Correa <leoneljaviercorrea.19@gmail.com>
2024-12-16 19:41:11 -0300

    Modificación del dalto de pagina del boton 'proyectos'

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function handleScreenSizeChange(event) {
 
         /* BOTON PROYECTOS */
         document.getElementById('proyectos').addEventListener('click', function() {
-            window.scrollTo({ top: 2800, behavior: 'smooth' });
+            window.scrollTo({ top: 2850, behavior: 'smooth' });
         });
 
         /* BOTON CONTACTO */
