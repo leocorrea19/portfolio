bacfcf7feb84970c5db4f152abe79e038fe4b0bb
Leonel Javier Correa <leoneljaviercorrea.19@gmail.com>
2024-12-16 19:37:27 -0300

    Actualización de los botones segun su version mobile o desktop

--- main.js
+++ main.js
@@ -1,26 +1,66 @@
 import './style.css'
 
-/* BOTON PARA VOLVER AL PRINCIPIO DE LA PAGINA - UBICADO EN LA PARTE INFERIOR DERECHA */
-document.getElementById('btn-para-arriba').addEventListener('click', function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
+const mediaQuery = window.matchMedia('(min-width: 1200px)');
 
-/* BOTON EDUCACION */
-document.getElementById('educacion').addEventListener('click', function() {
-    window.scrollTo({ top: 800, behavior: 'smooth' });
-});
+function handleScreenSizeChange(event) {
+    if (event.matches) {
+        // console.log("Tamaño desktop")
 
-/* BOTON TECNOLOGIAS */
-document.getElementById('tecnologias').addEventListener('click', function() {
-    window.scrollTo({ top: 1800, behavior: 'smooth' });
-});
+        /* BOTON PARA VOLVER AL PRINCIPIO DE LA PAGINA - UBICADO EN LA PARTE INFERIOR DERECHA */
+        document.getElementById('btn-para-arriba').addEventListener('click', function() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
 
-/* BOTON PROYECTOS */
-document.getElementById('proyectos').addEventListener('click', function() {
-    window.scrollTo({ top: 2700, behavior: 'smooth' });
-});
+        /* BOTON EDUCACION */
+        document.getElementById('educacion').addEventListener('click', function() {
+            window.scrollTo({ top: 1000, behavior: 'smooth' });
+        });
 
-/* BOTON CONTACTO */
-document.getElementById('contacto').addEventListener('click', function() {
-    window.scrollTo({ top: 3700, behavior: 'smooth' });
-});
+        /* BOTON TECNOLOGIAS */
+        document.getElementById('tecnologias').addEventListener('click', function() {
+            window.scrollTo({ top: 1850, behavior: 'smooth' });
+        });
+
+        /* BOTON PROYECTOS */
+        document.getElementById('proyectos').addEventListener('click', function() {
+            window.scrollTo({ top: 2800, behavior: 'smooth' });
+        });
+
+        /* BOTON CONTACTO */
+        document.getElementById('contacto').addEventListener('click', function() {
+            window.scrollTo({ top: 5000, behavior: 'smooth' });
+        });
+
+    } else {
+        // console.log("tamaño mobile")
+
+        /* BOTON PARA VOLVER AL PRINCIPIO DE LA PAGINA - UBICADO EN LA PARTE INFERIOR DERECHA */
+        document.getElementById('btn-para-arriba').addEventListener('click', function() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+
+        /* BOTON EDUCACION */
+        document.getElementById('educacion').addEventListener('click', function() {
+            window.scrollTo({ top: 800, behavior: 'smooth' });
+        });
+
+        /* BOTON TECNOLOGIAS */
+        document.getElementById('tecnologias').addEventListener('click', function() {
+            window.scrollTo({ top: 1800, behavior: 'smooth' });
+        });
+
+        /* BOTON PROYECTOS */
+        document.getElementById('proyectos').addEventListener('click', function() {
+            window.scrollTo({ top: 2700, behavior: 'smooth' });
+        });
+
+        /* BOTON CONTACTO */
+        document.getElementById('contacto').addEventListener('click', function() {
+            window.scrollTo({ top: 3700, behavior: 'smooth' });
+        });
+    }
+}
+
+handleScreenSizeChange(mediaQuery);
+
+mediaQuery.addEventListener('change', handleScreenSizeChange);
