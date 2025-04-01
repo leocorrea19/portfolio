c9aad6d0868c7385a1eafa2de3ab66038b8144c8
Leonel Javier Correa <leoneljaviercorrea.19@gmail.com>
2025-04-01 15:57:54 -0300

    Actualización de foto de perfil y correccion de errores

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ import './style.css'
 const mediaQuery = [window.matchMedia('(min-width: 1200px)'), window.matchMedia('(min-width: 768px)')];
 
 function handleScreenSizeChange(event) {
-    event.forEach(elemento => {
-        
+    for (const elemento of event) {
+        debugger
         if (elemento.media === '(min-width: 1200px)' && elemento.matches === true) {
             console.log("Tamaño 1200px o mas")
     
@@ -15,33 +15,59 @@ function handleScreenSizeChange(event) {
     
             /* BOTON EDUCACION */
             document.getElementById('educacion').addEventListener('click', function() {
-                window.scrollTo({ top: 1200, behavior: 'smooth' });
+                window.scrollTo({ top: 880, behavior: 'smooth' });
             });
     
             /* BOTON TECNOLOGIAS */
             document.getElementById('tecnologias').addEventListener('click', function() {
-                window.scrollTo({ top: 2260, behavior: 'smooth' });
+                window.scrollTo({ top: 1620, behavior: 'smooth' });
             });
     
             /* BOTON PROYECTOS */
             document.getElementById('proyectos').addEventListener('click', function() {
-                window.scrollTo({ top: 3350, behavior: 'smooth' });
+                window.scrollTo({ top: 2390, behavior: 'smooth' });
             });
     
             /* BOTON CONTACTO */
             document.getElementById('contacto').addEventListener('click', function() {
                 window.scrollTo({ top: 50000, behavior: 'smooth' });
             });
+            break
     
         }
-        
         else if(elemento.media === '(min-width: 768px)' && elemento.matches === true) {
             console.log("Tamaño 768px o mas")
+            debugger
+             /* BOTON PARA VOLVER AL PRINCIPIO DE LA PAGINA - UBICADO EN LA PARTE INFERIOR DERECHA */
+            document.getElementById('btn-para-arriba').addEventListener('click', function() {
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+            });
+    
+            /* BOTON EDUCACION */
+            document.getElementById('educacion').addEventListener('click', function() {
+                window.scrollTo({ top: 870, behavior: 'smooth' });
+            });
+    
+            /* BOTON TECNOLOGIAS */
+            document.getElementById('tecnologias').addEventListener('click', function() {
+                window.scrollTo({ top: 1900, behavior: 'smooth' });
+            });
+    
+            /* BOTON PROYECTOS */
+            document.getElementById('proyectos').addEventListener('click', function() {
+                window.scrollTo({ top: 2800, behavior: 'smooth' });
+            });
+    
+            /* BOTON CONTACTO */
+            document.getElementById('contacto').addEventListener('click', function() {
+                window.scrollTo({ top: 3700, behavior: 'smooth' });
+            });
+            break
         }
         
         else {
             console.log("tamaño menor a 768px")
-    
+            debugger
             /* BOTON PARA VOLVER AL PRINCIPIO DE LA PAGINA - UBICADO EN LA PARTE INFERIOR DERECHA */
             document.getElementById('btn-para-arriba').addEventListener('click', function() {
                 window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -49,30 +75,30 @@ function handleScreenSizeChange(event) {
     
             /* BOTON EDUCACION */
             document.getElementById('educacion').addEventListener('click', function() {
-                window.scrollTo({ top: 800, behavior: 'smooth' });
+                window.scrollTo({ top: 690, behavior: 'smooth' });
             });
     
             /* BOTON TECNOLOGIAS */
             document.getElementById('tecnologias').addEventListener('click', function() {
-                window.scrollTo({ top: 1800, behavior: 'smooth' });
+                window.scrollTo({ top: 1380, behavior: 'smooth' });
             });
     
             /* BOTON PROYECTOS */
             document.getElementById('proyectos').addEventListener('click', function() {
-                window.scrollTo({ top: 2700, behavior: 'smooth' });
+                window.scrollTo({ top: 1980, behavior: 'smooth' });
             });
     
             /* BOTON CONTACTO */
             document.getElementById('contacto').addEventListener('click', function() {
                 window.scrollTo({ top: 3700, behavior: 'smooth' });
             });
+            
         }
-    });
-    
+    }
 }
 
 handleScreenSizeChange(mediaQuery);
 
 console.log(mediaQuery)
 
-mediaQuery.addEventListener('change', handleScreenSizeChange);
+// mediaQuery.addEventListener('change', handleScreenSizeChange);
